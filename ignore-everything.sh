5c14d875db81024d22ace16eaa9480b5ff30de1b
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-09-15 17:02:48 -0400

    delete the turn format off script

--- ignore-everything.sh
+++
@@ -1,4 +0,0 @@
-for file in $(find soh \( -path soh/assets -prune -or -path soh/soh/Enhancements/randomizer/3drando -prune \) -name "*.cpp" -or -name "*.hpp" -or -name "*.c" -or -name "*.h"); do
-  cat format.off $file > $file.modified
-  mv $file.modified $file
-done
