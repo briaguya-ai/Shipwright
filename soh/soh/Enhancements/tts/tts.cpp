0f07a31664924ae8fe18c8b1c1469533abb98109
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-03-03 05:52:19 -0500

    bring back wip stuff

--- soh/soh/Enhancements/tts/tts.cpp
+++ soh/soh/Enhancements/tts/tts.cpp
@@ -1041,6 +1041,10 @@ void InitTTSBank() {
     if (sceneFile != nullptr) {
         sceneMap = nlohmann::json::parse(*sceneFile->Buffer.get(), nullptr, true, true);
     }
+    // use binaryreader as base for rawjsonfactory
+    // just pass a shared pointer to initdata in here
+    // sceneMap = LUS::Context::GetInstance()->GetResourceManager()->LoadResource("accessibility/texts/scenes" + languageSuffix, initdata);
+ 
     
     auto miscFile = LUS::Context::GetInstance()->GetResourceManager()->GetArchiveManager()->LoadFileRaw("accessibility/texts/misc" + languageSuffix);
     if (miscFile != nullptr) {
